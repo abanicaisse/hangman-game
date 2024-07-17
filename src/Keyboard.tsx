1d3dcbdc7a017586f49163fe2253082f278ee8ba
Aba Nicaisse <abawandjovunicaise@gmail.com>
2024-07-17 19:40:39 +0300

    Rendered Keyboard

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,7 +1,53 @@
 import React from "react";
 
+import styles from "./keyboard.module.css";
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 const Keyboard = () => {
-  return <div>Keyboard</div>;
+  return (
+    <div
+      className="keyboard"
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => (
+        <button key={key} className={`${styles.btn}`}>
+          {key}
+        </button>
+      ))}
+    </div>
+  );
 };
 
 export default Keyboard;
